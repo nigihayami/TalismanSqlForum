315357b3efb5c70fc306ecca6f6a924151678866
nigihayami <hotaruhiro@gmail.com>
2014-08-26 15:04:15 +0400

    Еще немного КОНЧЕННОЙ статистики

--- TalismanSqlForum/Code/Stat.cs
+++ TalismanSqlForum/Code/Stat.cs
@@ -20,32 +20,69 @@ namespace TalismanSqlForum.Code
         public static int count_mess(string username, int id)
         {
             using (ApplicationDbContext db = new ApplicationDbContext())
-            {                
+            {
                 var t = db.tUserNewThemes.Where(a => a.tUsers.UserName == username).Where(b => b.tForumThemes.Id == id).Count();
                 return t;
             }
         }
-
-        public class Stat_Forum
+        public class statForumList
         {
-            public int tForumThemes_Count { get; set; }
-            public int tForumMessages_Count { get; set; }
-            public int ApplicationUser_Count { get; set; }
-            public string LastUser { get; set; }
-            public int Max_User_login_count { get; set; }
-            public string Date_User_login_count { get; set; }
+            public int count_list { get; set; }
+            public int count_NewForumThemes { get; set; }
+            public int count_NewForumMessages { get; set; }
+            public int count_OldForumThemes { get; set; }
+            public int count_OldForumMessages { get; set; }
+            public int count_View { get; set; }
+            public int count_Users { get; set; }
+            public int count_ForumMessages { get; set; }
+            public int count_ForumThemes { get; set; }
+            public string mega_User { get; set; }
         }
-        public static Stat_Forum get_stat()
+        //Статистика форума
+        public static statForumList Stat_Forum()
         {
-            var t = new Stat_Forum();
+            var t = new statForumList();
             using (ApplicationDbContext db = new ApplicationDbContext())
             {
-                t.tForumMessages_Count = db.tForumMessages.Count();
-                t.tForumThemes_Count = db.tForumThemes.Count();
-                t.ApplicationUser_Count = db.Users.Count();
-                t.LastUser = db.Users.OrderByDescending(a => a.DateReg).First().UserName;
-                //t.Max_User_login_count = db.Users.GroupBy(a => a.LastIn.ToString("ddMMY")).Max(a => a.Count());
-                //t.Date_User_login_count = db.Users.GroupBy(a => a.LastIn.ToString("ddMMY")).Max(a => a.Count()).ToString();
+                var d = DateTime.Now.AddDays(-1);
+                var d2 = DateTime.Now.AddDays(-2);
+                t.count_list = db.tForumLists.Where(a => !a.tForumList_hide).Count();
+                t.count_NewForumThemes = db.tForumThemes.Where(a => a.tForumThemes_datetime >= d).Count();
+                t.count_NewForumMessages = db.tForumMessages.Where(a => a.tForumMessages_datetime >= d).Count();
+                t.count_OldForumThemes = db.tForumThemes.Where(a => a.tForumThemes_datetime >= d2).Where(b => b.tForumThemes_datetime <= d).Count();
+                t.count_OldForumMessages = db.tForumMessages.Where(a => a.tForumMessages_datetime >= d2).Where(b => b.tForumMessages_datetime <= d).Count();
+                t.count_View = 0; //я пока не знаю как это сделать
+                t.count_Users = db.Users.Count();
+                t.count_ForumMessages = db.tForumMessages.Count();
+                t.count_ForumThemes = db.tForumThemes.Count();
+                var i = 0;
+                foreach (var item in db.tForumMessages.GroupBy(a => a.tUsers).OrderByDescending(a => a.Count()))
+                {
+                    t.mega_User += item.First().tUsers.UserName;
+                    i++;
+                    if (i == 3)
+                        break;
+                }
+                db.Dispose();
+            }
+            return t;
+        }
+        //Статистика форумаID
+        public static statForumList Stat_ForumList(int id)
+        {
+            var t = new statForumList();
+            using (ApplicationDbContext db = new ApplicationDbContext())
+            {
+                var d = DateTime.Now.AddDays(-1);
+                var d2 = DateTime.Now.AddDays(-2);
+                t.count_NewForumThemes = db.tForumThemes.Where(a => a.tForumList.Id == id).Where(a => a.tForumThemes_datetime >= d).Count();
+                t.count_NewForumMessages = db.tForumMessages.Where(a => a.tForumThemes.tForumList.Id == id).Where(a => a.tForumMessages_datetime >= d).Count();
+                t.count_OldForumThemes = db.tForumThemes.Where(a => a.tForumList.Id == id).Where(a => a.tForumThemes_datetime >= d2).Where(b => b.tForumThemes_datetime <= d).Count();
+                t.count_OldForumMessages += db.tForumMessages.Where(a => a.tForumThemes.tForumList.Id == id).Where(a => a.tForumMessages_datetime >= d2).Where(b => b.tForumMessages_datetime <= d).Count();
+                t.count_View = 0; //я пока не знаю как это сделать
+                t.count_Users = db.tForumMessages.Where(a => a.tForumThemes.tForumList.Id == id).GroupBy(a => a.tUsers).Count();
+                t.count_ForumMessages = db.tForumMessages.Where(a => a.tForumThemes.tForumList.Id == id).Count();
+                t.count_ForumThemes = db.tForumThemes.Where(a => a.tForumList.Id == id).Count();
                 db.Dispose();
             }
             return t;
---
+++ TalismanSqlForum/Controllers/Admin/RulesController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using TalismanSqlForum.Models;
+using TalismanSqlForum.Models.Admin;
+
+namespace TalismanSqlForum.Controllers.Admin
+{
+    [Authorize(Roles="admin")]
+    public class RulesController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: Rules
+        [AllowAnonymous]
+        public ActionResult Rules()
+        {
+            return View(db.tRules.First());
+        }
+
+        public ActionResult Edit()
+        {
+            var t = db.tRules.First();
+            return View(t);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,tRules_rules")] tRules trules)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(trules).State = System.Data.Entity.EntityState.Modified;
+                db.SaveChanges();
+                RedirectToAction("Index");
+            }
+            return View(trules);
+        }
+    }
+}
