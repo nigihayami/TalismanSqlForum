543755629c62ad895c9fa5ab2d22f215505a4c32
nigihayami <hotaruhiro@gmail.com>
2014-08-26 16:45:30 +0400

    Немного видоизменен результат поиска

--- TalismanSqlForum/Controllers/SearchController.cs
+++ TalismanSqlForum/Controllers/SearchController.cs
@@ -12,13 +12,24 @@ namespace TalismanSqlForum.Controllers
         private ApplicationDbContext db = new ApplicationDbContext();
 
         // GET: Search
-        public ActionResult Index(string val)
+        public ActionResult Index(string _searchVal, string _searchUser)
         {
-            ViewData["val"] = val;
-            ViewData["tForumThemes"] = db.tForumThemes.Where(a => a.tForumThemes_name.Contains(val));
-            ViewData["tForumThemes_desc"] = db.tForumThemes.Where(a => a.tForumThemes_desc.Contains(val));
-            ViewData["tForumMessages"] = db.tForumMessages.Where(a => a.tForumMessages_messages.Contains(val));
-            return View();
+            ViewData["val"] = _searchVal;
+            var t1 = db.tForumThemes.Where(a => a.tForumThemes_name.Contains(_searchVal));
+            var t2 = db.tForumThemes.Where(a => a.tForumThemes_desc.Contains(_searchVal));
+            var t3 = db.tForumMessages.Where(a => a.tForumMessages_messages.Contains(_searchVal));
+            if (_searchUser != null && _searchUser != "")
+            {
+                t1 = t1.Where(a => a.tUsers.UserName.ToUpper() == _searchUser);
+                t2 = t2.Where(a => a.tUsers.UserName.ToUpper() == _searchUser);
+                t3 = t3.Where(a => a.tUsers.UserName.ToUpper() == _searchUser);
+            }
+
+            ViewData["user"] = _searchUser;
+            ViewData["tForumThemes"] = t1;
+            ViewData["tForumThemes_desc"] = t2;
+            ViewData["tForumMessages"] = t3;
+            return View("Index");
         }
     }
 }
