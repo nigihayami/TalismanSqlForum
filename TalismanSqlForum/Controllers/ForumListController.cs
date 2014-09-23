9f6219819397d07e49261a702d870fd5ec840bd6
nigihayami <hotaruhiro@gmail.com>
2014-09-23 11:17:55 +0400

    Сокрытие всех тем на конкретном форуме

--- TalismanSqlForum/Controllers/ForumListController.cs
+++ TalismanSqlForum/Controllers/ForumListController.cs
@@ -21,6 +21,23 @@ namespace TalismanSqlForum.Controllers
             ViewData["tForumList"] = db.tForumLists.Where(a=> !a.tForumList_hide).OrderBy(a => a.tForumList_name).ToList();
             return View();
         }
+
+        [Authorize(Roles="admin")]
+        public ActionResult HideAllThemes(int? id)
+        {
+            var t = db.tForumLists.Find(id);
+            if (t != null)
+            {
+                foreach (var item in t.tForumThemes)
+                {
+                    item.tForumThemes_hide = true;
+                    db.Entry(item).State = EntityState.Modified;
+                    db.SaveChanges();
+                }
+                return RedirectToAction("Index", "ForumThemes", new { id = id });
+            }
+            return HttpNotFound();
+        }
         protected override void Dispose(bool disposing)
         {
             if (disposing)
